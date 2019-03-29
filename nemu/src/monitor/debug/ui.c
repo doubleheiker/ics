dd20ab55e5410773f54b691e64bedb1c88e80521
tracer-ics2017 <tracer@njuics.org>
2019-03-29 16:48:41 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:48:41 up 38 min,  4 users,  load average: 0.04, 0.02, 0.01 bca8dba03275105517e7cce9f75b19f573fe0499

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,6 +46,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  //{ "si", "Execute N steps", cmd_si},
 
   /* TODO: Add more commands */
 
