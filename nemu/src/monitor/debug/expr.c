a9f52bd5c8c7926329bc25ec032f8124baf539bc
tracer-ics2017 <tracer@njuics.org>
2019-04-12 14:07:09 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  14:07:09 up  5:08,  3 users,  load average: 0.20, 0.06, 0.01 3a193c0f9d2f8e7dc8d79937c0434dc06d030f75

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -87,6 +87,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
+		scanf("nothing wrong here");
 
         switch (rules[i].token_type) {
 			case TK_HEX:
