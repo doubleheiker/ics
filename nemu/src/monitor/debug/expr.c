2984c18c14b6663be75281de568290b8938a3b47
tracer-ics2017 <tracer@njuics.org>
2019-04-12 15:43:36 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:43:36 up  6:44,  3 users,  load average: 0.13, 0.03, 0.01 d9d7989a47c33538836d87efc0e490893072d12d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -115,7 +115,6 @@ static bool make_token(char *e) {
 			case TK_HEX:
 				tokens[nr_token].type = TK_HEX;
 				strncpy(tokens[nr_token].str, substr_start, substr_len);
-				scanf("%s", tokens[nr_token].str);
 				nr_token++;
 				break;
 			case TK_DEC:
