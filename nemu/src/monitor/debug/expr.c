65edf866e2f4f5ebe68927ab005c173df3198ffc
tracer-ics2017 <tracer@njuics.org>
2019-04-13 16:30:34 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:30:34 up  6:07,  3 users,  load average: 0.00, 0.01, 0.00 ef0fd96e4d61e0ef3c5fe155f1d62728e13fa576

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -276,7 +276,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
+  printf("entered expr");
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
   *success = true;
