cce5bddaa69d7dea8df3e01f4ab43afb80ba0882
tracer-ics2017 <tracer@njuics.org>
2019-04-14 02:00:53 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  02:00:53 up 15:38,  3 users,  load average: 0.10, 0.06, 0.01 ddbc5605ed5442ddc44d05a7335c32a74d6bf35

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -244,6 +244,7 @@ uint32_t eval(int p, int q) {
 		else if (tokens[p].type == TK_REG) {
 			//int ret;
 			//ret = strncmp(tokens[p].str, "$eax", 4);
+			printf("%s", tokens[p].str);
 			if (1) {
 				res = cpu.eax;
 				return res;
