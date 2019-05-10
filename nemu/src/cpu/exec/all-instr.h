78240a97bf296b9d9544513e346a4ad606af330e
tracer-ics2017 <tracer@njuics.org>
2019-05-10 21:57:33 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  21:57:33 up  1:49,  2 users,  load average: 0.00, 0.00, 0.00 4872312fdcf4512a6962a40c5dab926b7e3fcb94

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,6 +1,5 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
 
 make_EHelper(operand_size);
 
@@ -9,11 +8,12 @@ make_EHelper(inv);
 make_EHelper(nemu_trap);
 
 /*control.c*/
+make_EHelper(jcc);
 make_EHelper(call);
 make_EHelper(ret);
 
 /*data-mov.c*/
-//make_EHelper(mov);
+make_EHelper(mov);
 make_EHelper(push);
 make_EHelper(pop);
 make_EHelper(lea);
