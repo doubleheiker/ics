cd4ab9ade97c53fe5d70724070d294717448d250
tracer-ics2017 <tracer@njuics.org>
2019-05-13 22:08:56 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:08:56 up  3:26,  5 users,  load average: 0.08, 0.02, 0.01 453b5e83759a5a5ac7c71d043e932f530d04f8a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -48,22 +48,32 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   //TODO();
-  rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
+  //rtl_sub(&t2, &id_dest->val, &id_src->val);
+  //rtl_sltu(&t3, &id_dest->val, &t2);
   //set CF
-  rtl_set_CF(&t3);
+  //rtl_set_CF(&t3);
 
   //set ZF SF
-  rtl_update_ZFSF(&t2, id_dest->width);
-  printf("SF:%d ZF:%d\n", cpu.eflags.SF, cpu.eflags.ZF);
+  //rtl_update_ZFSF(&t2, id_dest->width);
+  //printf("SF:%d ZF:%d\n", cpu.eflags.SF, cpu.eflags.ZF);
 
   //set OF
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t0, &id_dest->val, &t2);
-  rtl_and(&t1, &t0, &t2);
-  rtl_msb(&t0, &t0, id_dest->width);
-  rtl_set_OF(&t0);
-  printf("OF:%d\n", cpu.eflags.OF);
+  //rtl_xor(&t0, &id_dest->val, &id_src->val);
+  //rtl_xor(&t0, &id_dest->val, &t2);
+  //rtl_and(&t1, &t0, &t2);
+  //rtl_msb(&t0, &t0, id_dest->width);
+  //rtl_set_OF(&t0);
+  //printf("OF:%d\n", cpu.eflags.OF);
+  rtl_sext(&t1, &id_dest->val, id_dest->width);
+  rtl_sext(&t2, &id_src->val, id_src->width);
+
+  rtl_sub(&t0, &t1, &t2);
+  t3 = (t0 > t1);
+  rtl_set_CF(&t3);
+  t3 = ((((int32_t)(t1)<0) == (((int32_t)(t2)>>31)==0))&&(((int32_t)(t0)<0)!=((int32_t)(t1)<0)));
+  rtl_set_OF(&t3);
+  rtl_update_ZFSF(&t0, 4);
+  printf("SF:%d OF:%d\nZF:%d\n", cpu.eflags.SF, cpu.eflags.OF, cpu.eflags.ZF);
 
   print_asm_template2(cmp);
 }
