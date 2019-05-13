f1f8553fc6a04ddb47a58af4bc69eb349a195737
tracer-ics2017 <tracer@njuics.org>
2019-05-13 19:39:38 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:39:38 up 56 min,  2 users,  load average: 0.09, 0.02, 0.01 e307d47b2c118616ec0c07dd1ab63e96c930a642

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -67,7 +67,21 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-  TODO();
+  //TODO();
+  t3 = 1;
+  rtl_add(&t2, &id_dest->val, &t3);
+  operand_write(id_dest, &t2);
+
+  //set SFZF
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  // set OF
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(inc);
 }
