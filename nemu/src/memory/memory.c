a34b2a06605f38f1b792429628e83b299f8802b9
tracer-ics2017 <tracer@njuics.org>
2019-05-16 20:45:24 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  20:45:24 up 21 min,  3 users,  load average: 0.00, 0.00, 0.00 cfdfe05d33caf5a3af89df13da1919419925aef3

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -23,7 +23,7 @@ uint32_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
   int mmio_id = is_mmio(addr);
-  if (mmio_id != 1) {
+  if (mmio_id != -1) {
 	  mmio_write(addr, len, data, mmio_id);
   }
   else memcpy(guest_to_host(addr), &data, len);
