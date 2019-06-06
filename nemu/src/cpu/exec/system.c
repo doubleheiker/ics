ecd06b06ef42e7601ab0bd266fd92d9ac18faa2b
tracer-ics2017 <tracer@njuics.org>
2019-06-06 18:26:13 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  18:26:13 up  2:24,  2 users,  load average: 0.00, 0.00, 0.00 d83f5423c38fabf5b9609cd4ab3ab1279b0441fd

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -7,14 +7,20 @@ void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
   //TODO();
-  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
+  /*cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
   if (decoding.is_operand_size_16) {
 	  cpu.idtr.base = vaddr_read(id_dest->addr+2, 3);
   }
   else cpu.idtr.base = vaddr_read(id_dest->addr+2, 4);
   
-  printf("base: 0x%08x, limit: 0x%08x\n", cpu.idtr.base, cpu.idtr.limit);
-  print_asm_template1(lidt);
+  //printf("base: 0x%08x, limit: 0x%08x\n", cpu.idtr.base, cpu.idtr.limit);
+  print_asm_template1(lidt);*/
+  rtl_lm(&t0, &id_dest->addr, 2); 
+  cpu.idtr.limit = t0;//先取低16位设置limit
+  t1 = id_dest->addr + 2;//跳过limit 
+  rtl_lm(&t0, &t1, 4);//取高32位 
+  cpu.idtr.base = (t0 & (decoding.is_operand_size_16 ? 0x00FFFFFF : 0xFFFFFFFF));//去掉高8位     
+  print_asm_template1(lidt); 
 }
 
 make_EHelper(mov_r2cr) {
