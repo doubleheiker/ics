5b69f72139d17fe2e966916727be278952d0d3b9
tracer-ics2017 <tracer@njuics.org>
2019-06-06 19:57:26 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:57:26 up  3:55,  2 users,  load average: 0.07, 0.05, 0.01 c71f842791547bd1591ef4ba352094513cc80f

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,7 +9,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   //TODO();
   vaddr_t addr;
   addr = NO * 8 + cpu.idtr.base;
-  printf("NO: %d, base: 0x%08x, addr: 0x%08x\n", NO, cpu.idtr.base, addr);
+  //printf("NO: %d, base: 0x%08x, addr: 0x%08x\n", NO, cpu.idtr.base, addr);
 
   union {
 	  GateDesc gd;
@@ -21,12 +21,12 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   t.low = vaddr_read(addr, 4);
   t.high = vaddr_read(addr + 4, 4);
 
-  printf("t.low: 0x%08x, t.high: 0x%08x\n", t.low, t.high);
+  //printf("t.low: 0x%08x, t.high: 0x%08x\n", t.low, t.high);
 
-  /*if (t.gd.present == 0) {
+  if (t.gd.present == 0) {
 	  printf("invaild P");
 	  assert(0);//invaild p
-  }*/
+  }
 
   rtl_push(&cpu.EFLAGS);
   t0 = cpu.cs;
@@ -35,7 +35,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   
   decoding.is_jmp = 1;
   decoding.jmp_eip = (t.gd.offset_15_0 & 0xffff) | ((t.gd.offset_31_16 & 0xffff)<<16);
-  printf("off15: %x\noff31: %x\njmp: %x\n", t.gd.offset_15_0, t.gd.offset_31_16, decoding.jmp_eip);
+  //printf("off15: %x\noff31: %x\njmp: %x\n", t.gd.offset_15_0, t.gd.offset_31_16, decoding.jmp_eip);
   cpu.eflags.IF = 0; 
 }
 
