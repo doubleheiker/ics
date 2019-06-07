586706fb79a563774b838071a834ef0d0147a290
tracer-ics2017 <tracer@njuics.org>
2019-06-07 15:35:45 +0800

    >  run 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:35:45 up  5:26,  4 users,  load average: 0.00, 0.07, 0.05 d544fe73feb82e91cd47f23c81d0430932753854

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -54,7 +54,7 @@ _RegSet* do_syscall(_RegSet *r) {
 	case SYS_exit: _halt(a[1]);
 				   break;
 	case SYS_write: SYSCALL_ARG1(r) = sys_write((uintptr_t)a[1], (uintptr_t)a[2], (uintptr_t)a[3]);
-					//Log("write");
+					Log("write");
 					break;
 	case SYS_brk: SYSCALL_ARG1(r) = sys_brk((uintptr_t)a[1]);
 				  Log("brk");
