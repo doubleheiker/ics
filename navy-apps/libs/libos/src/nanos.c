93d925468423a9a96e4a70deadbada7d5671b7bb
tracer-ics2017 <tracer@njuics.org>
2019-06-07 16:23:42 +0800

    >  run 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:23:42 up  6:14,  4 users,  load average: 0.00, 0.00, 0.00 4902f518af46319fc5e786214490863623bbf403

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -11,7 +11,7 @@
 
 // FIXME: this is temporary
 
-extern char end;
+extern char _end;
 
 int _syscall_(int type, uintptr_t a0, uintptr_t a1, uintptr_t a2){
   int ret = -1;
@@ -32,14 +32,14 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  static intptr_t brk = &end;
+  static intptr_t brk = &_end;
   intptr_t old_brk = brk;
-  int i = _syscall_(SYS_brk, old_brk+increment, 0, 0);
-  printf("syscall: %d", i);
+  //int i = _syscall_(SYS_brk, old_brk+increment, 0, 0);
+  //printf("syscall: %d", i);
   if (_syscall_(SYS_brk, old_brk+increment, 0, 0) == 0) {
-	  Log("in brk");
+	  //Log("in brk");
 	  brk = increment + old_brk;
-	  return (void *)oldbrk;
+	  return (void *)old_brk;
   }
   return (void *)-1;
 }
