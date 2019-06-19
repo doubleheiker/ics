c7d4a9b054853892525953224a5b4e9ae7641ed1
tracer-ics2017 <tracer@njuics.org>
2019-06-19 22:20:29 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:20:29 up  2:11,  2 users,  load average: 0.02, 0.08, 0.04 85d0dd0ad7ff4723527f2362fd7e09d725fc074c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -52,7 +52,7 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
 
 	pt = pde.page_frame << 12;
 	pte.val = paddr_read((pt + ((addr >> 12) & 0x3ff)), 4);
-	Log("present: %d", pte.present);
+	Log("pte: %x\tpresent: %d", pte.val, pte.present);
 	assert(pte.present);
 	pte.accessed = 1;
 	pte.dirty = is_write ? 1 : pte.dirty;
