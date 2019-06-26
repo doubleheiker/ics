97f7e4bf34095a609cc9d7a7380d40dbc027798f
tracer-ics2017 <tracer@njuics.org>
2019-06-26 21:52:00 +0800

    >  compile 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  21:52:00 up  4:05,  2 users,  load average: 0.08, 0.03, 0.01 24d39fa17001802f50af90deef9e782cdc408a51

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,10 +6,13 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   _RegSet *ret = NULL;
   switch (e.event) {
-	case _EVENT_SYSCALL: ret = do_syscall(r);
+	case _EVENT_SYSCALL: do_syscall(r);
 						 break;
 	case _EVENT_TRAP: ret = schedule(r);
 					  break;
+	case _EVENT_IRQ_TIME: Log("IRQ_TIME!");
+						  ret = schedule(r);
+						  break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
