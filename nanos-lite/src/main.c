b21fff5572e6c95b74e09732795b80e92b76ea0a
tracer-ics2017 <tracer@njuics.org>
2019-06-26 22:00:27 +0800

    >  run 161720127 yangke Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:00:27 up  4:14,  2 users,  load average: 0.18, 0.28, 0.17 3cd0ef5cd7b3b02bbb2e66cc2f58737c7a032766

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -36,9 +36,9 @@ int main() {
   //((void (*)(void))entry)();
   //load_prog("/bin/dummy");
   //load_prog("/bin/text");
-  //load_prog("/bin/pal");
-  load_prog("/bin/hello");
   load_prog("/bin/pal");
+  load_prog("/bin/hello");
+  //load_prog("/bin/pal");
 
   _trap();
 
